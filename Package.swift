de4e02c23834518cbcb46106942cda1f00dbaf40
Bernardo Silva <silva98.bernardo@gmail.com>
2020-07-02 14:42:13 -0300

    package version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,10 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SPM-Example",
-            dependencies: [],
-            resources: [
-                .process("spm.jpg")
-            ]),
+            dependencies: []
+//            resources: [
+//                .process("spm.jpg")
+//            ]
+        ),
         .testTarget(
             name: "SPM-ExampleTests",
             dependencies: ["SPM-Example"]),
